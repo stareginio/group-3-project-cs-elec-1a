65bb764aad6767a97cddd69d6bc345ba22903030
stareginio <sophiecodes02@gmail.com>
2022-12-09 18:41:30 +0800

    Update grp3_naive_bayes.py
    modified cleanData(), fixed elimination of outliers

--- grp3_naive_bayes.py
+++ grp3_naive_bayes.py
@@ -6,6 +6,7 @@ from scipy import stats
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+from sklearn.preprocessing import MinMaxScaler
 
 # == DATA LOADING ============================================================================
 dataset_csv = "weatherAUS"
@@ -15,7 +16,7 @@ dataset = pd.read_csv(f"{dataset_csv}.csv")
 # print(dataset.shape)
 
 X = dataset.iloc[:,:-1]     # all columns except the last, RainTomorrow
-y = dataset['RainTomorrow']
+y = dataset['RainTomorrow'].to_frame()  # RainTomorrow
 
 # == DATA VISUALIZATION ======================================================================
 # print("X:")
@@ -27,9 +28,9 @@ y = dataset['RainTomorrow']
 
 # == DATA SPLITTING ==========================================================================
 # set random_state for reproducibility
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=9)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=41)
 
-print("Train shapes")
+print("---- Train shapes")
 print("X_train shape:")
 print(X_train.shape)
 print("y_train shape:")
@@ -43,7 +44,7 @@ print(y_test.shape)
 
 # == DATA CLEANING ===========================================================================
 # function for data cleaning
-def cleanData(X_cols, y_col):
+def cleanData(set):
     # -- drop variables that are irrelevant and has many NA values -------------------------------
     to_drop = [
         'Evaporation',
@@ -53,47 +54,37 @@ def cleanData(X_cols, y_col):
         'Location',
         'Date'
     ]
-    X_cols.drop(to_drop, inplace=True, axis=1)
+    set.drop(to_drop, inplace=True, axis=1)
 
     # -- drop records with NA values under RainToday ---------------------------------------------
-    X_cols.dropna(subset=['RainToday'], inplace=True)
+    set.dropna(subset=['RainToday'], inplace=True)
 
-    print("\nAfter dropping records")
-    print(X_cols.head())
+    print("\n---- After dropping records")
+    print(set.head())
     print("shape:")
-    print(X_cols.shape)
+    print(set.shape)
 
     # -- label encode non-numeric variables ------------------------------------------------------
-    X_cols[[
+    columns = [
         'WindGustDir',
         'WindDir9am',
         'WindDir3pm',
-        'RainToday'
-        ]] = X_cols[[
-        'WindGustDir',
-        'WindDir9am',
-        'WindDir3pm',
-        'RainToday'
-        ]].apply(lambda x: pd.factorize(x)[0])
+        'RainToday',
+        'RainTomorrow'
+    ]
+    set[columns] = set[columns].apply(lambda x: pd.factorize(x)[0])
 
-    y_col = pd.factorize(y_col)[0]
-
-
-    print("\nAfter label-encoding")
-    print("X_cols:")
+    print("\n---- After label-encoding")
     print("\nhead:")
-    print(X_cols.head())
-    print("\ntail:")
-    print(X_cols.tail())
+    print(set.head())
+    # print("\ntail:")
+    # print(set.tail())
     print("shape:")
-    print(X_cols.shape)
-
-    print("\ny_train:")
-    print(y_col)
+    print(set.shape)
 
     # -- impute missing values -------------------------------------------------------------------
     # using mode under RainTomorrow
-    y_col['RainTomorrow'].fillna(y_col['RainTomorrow'].mode(), inplace=True)
+    set['RainTomorrow'].fillna(set['RainTomorrow'].mode(), inplace=True)
 
     # using mean under variables (except RainToday and RainTomorrow)
     columns = [
@@ -102,34 +93,37 @@ def cleanData(X_cols, y_col):
         'WindSpeed3pm', 'Humidity9am', 'Humidity3pm', 'Pressure9am',
         'Pressure3pm', 'Temp9am', 'Temp3pm'
     ]
-    X_cols[columns] = X_cols[columns].fillna(X_cols[columns].mean())
+    set[columns] = set[columns].fillna(set[columns].mean())
 
-    print("\nAfter imputing missing values")
-    print("X_cols:")
+    print("\n---- After imputing missing values")
     print("\nhead:")
-    print(X_cols.head())
-    print("\ntail:")
-    print(X_cols.tail())
-    print("shape:")
-    print(X_cols.shape)
-
-    print("\ny_train:")
-    print(y_col)
-
-    # work in progress...
-    # -- detect and eliminate outliers using z-score ---------------------------------------------
-    # threshold = 3  # drop records greater than this value
-    # X_cols[(np.abs(stats.zscore(X_cols)) < threshold).all(axis=1)]
-
-    # print("\nAfter eliminating outliers")
-    # print("\nhead:")
-    # print(X_cols.head())
+    print(set.head())
     # print("\ntail:")
-    # print(X_cols.tail())
-    # print("shape:")
-    # print(X_cols.shape)
+    # print(set.tail())
+    print("shape:")
+    print(set.shape)
+
+    # -- detect and eliminate outliers using z-score ---------------------------------------------
+    threshold = 3  # drop records greater than this value
+    set = set[(np.abs(stats.zscore(set)) < threshold).all(axis=1)]
+
+    print("\n---- After eliminating outliers")
+    print("\nhead:")
+    print(set.head())
+    # print("\ntail:")
+    # print(set.tail())
+    print("shape:")
+    print(set.shape)
+
+    return set
+
+# combine X_train and y_train for data cleaning
+train_set = pd.concat([X_train,y_train], axis=1)
+
+print("\n---- Train set after concatenating X_train and y_train")
+print(train_set)
 
 # call the function for data cleaning
-print("Data cleaning for training set:")
-cleanData(X_train, y_train)
-
+train_set = cleanData(train_set)
+print("\n---- After data cleaning for training set:")
+print(train_set)
