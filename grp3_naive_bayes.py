8b4eab0b831cc913624b517fc4af146ca707759a
stareginio <sophiecodes02@gmail.com>
2022-12-10 02:40:41 +0800

    Update grp3_naive_bayes.py

--- grp3_naive_bayes.py
+++ grp3_naive_bayes.py
@@ -1,12 +1,10 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 from scipy import stats
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import confusion_matrix, classification_report
 
 # == DATA LOADING ================================================================================
 # in local machine
@@ -35,21 +33,35 @@ y = dataset['RainTomorrow'].to_frame()  # RainTomorrow
 # set random_state for reproducibility
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=41)
 
-print("---- Train shapes")
-print("X_train shape:")
-print(X_train.shape)
-print("y_train shape:")
-print(y_train.shape)
+# print("---- Train shapes")
+# print("X_train shape:")
+# print(X_train.shape)
+# print("y_train shape:")
+# print(y_train.shape)
 
-print("Test shapes")
-print("X_test shape:")
-print(X_test.shape)
-print("y_test shape:")
-print(y_test.shape)
+# print("Test shapes")
+# print("X_test shape:")
+# print(X_test.shape)
+# print("y_test shape:")
+# print(y_test.shape)
 
 # == DATA CLEANING ===============================================================================
+# combine X_train and y_train for data cleaning
+train_set = pd.concat([X_train,y_train], axis=1)
+# print("\n---- Train set after concatenating X_train and y_train")
+# print(train_set)
+
+# parameters to be used for test set later
+mode_rt = None
+mean = None
+mode_wgd = None
+mode_wdn = None
+mode_wdt = None
+
 # function for data cleaning
-def cleanData(set):
+def cleanData(set, mode_rt, mean, mode_wgd, mode_wdn, mode_wdt, type_set):
+    print(f"\ntype_set: {type_set} ===============")
+    
     # -- drop variables that are irrelevant and has many NA values -------------------------------
     to_drop = [
         'Evaporation',
@@ -64,80 +76,140 @@ def cleanData(set):
     # -- drop records with NA values under RainToday ---------------------------------------------
     set.dropna(subset=['RainToday'], inplace=True)
 
-    print("\n---- After dropping records")
-    print(set.head())
-    print("shape:")
-    print(set.shape)
+    # print("\n---- After dropping records")
+    # print(set.head(20))
+    # print("shape:")
+    # print(set.shape)
 
     # -- label encode non-numeric variables ------------------------------------------------------
-    columns = [
+    le_columns = [
         'WindGustDir',
         'WindDir9am',
         'WindDir3pm',
         'RainToday',
         'RainTomorrow'
     ]
-    set[columns] = set[columns].apply(lambda x: pd.factorize(x)[0])
+    set[le_columns] = set[le_columns].apply(lambda x: pd.factorize(x)[0])
 
-    print("\n---- After label-encoding")
-    print("\nhead:")
-    print(set.head())
-    # print("\ntail:")
-    # print(set.tail())
-    print("shape:")
-    print(set.shape)
+    # print("\n---- After label-encoding")
+    # print("\nhead:")
+    # print(set.head(20))
+    # # print("\ntail:")
+    # # print(set.tail())
+    # print("shape:")
+    # print(set.shape)
 
     # -- impute missing values -------------------------------------------------------------------
     # using mode under RainTomorrow
-    set['RainTomorrow'].fillna(set['RainTomorrow'].mode(), inplace=True)
+    if (type_set == 'train'):
+        mode_rt = set['RainTomorrow'].mode()[0]
+        # print(f'mode_rt for train: {mode_rt}')
+    set['RainTomorrow'] = set['RainTomorrow'].replace(-1, mode_rt)
 
     # using mean under variables (except RainToday and RainTomorrow)
-    columns = [
+    imp_columns = [
         'MinTemp', 'MaxTemp', 'Rainfall', 'WindGustDir',
         'WindGustSpeed', 'WindDir9am', 'WindDir3pm', 'WindSpeed9am',
         'WindSpeed3pm', 'Humidity9am', 'Humidity3pm', 'Pressure9am',
         'Pressure3pm', 'Temp9am', 'Temp3pm'
     ]
-    set[columns] = set[columns].fillna(set[columns].mean())
+    if (type_set == 'train'):
+        mean = set[imp_columns].mean()
+        mode_wgd = set['WindGustDir'].mode()[0]
+        mode_wdn = set['WindDir9am'].mode()[0]
+        mode_wdt = set['WindDir3pm'].mode()[0]
+        # print(f'mean for train: {mean}')
+        # print(f'mode_wgd for train: {mode_wgd}')
+        # print(f'mode_wdn for train: {mode_wdn}')
+        # print(f'mode_wdt for train: {mode_wdt}')
+    set[imp_columns] = set[imp_columns].fillna(mean)
 
-    print("\n---- After imputing missing values")
-    print("\nhead:")
-    print(set.head())
-    # print("\ntail:")
-    # print(set.tail())
-    print("shape:")
-    print(set.shape)
+    # using mode under WindGustDir, WindDir9am, WindDir3pm to impute encoded NaN values (i.e., -1)
+    set['WindGustDir'] = set['WindGustDir'].replace(-1, mode_wgd)
+    set['WindDir9am'] = set['WindDir9am'].replace(-1, mode_wdn)
+    set['WindDir3pm'] = set['WindDir3pm'].replace(-1, mode_wdt)
 
-    # -- detect and eliminate outliers using z-score ---------------------------------------------
-    threshold = 3  # drop records greater than this value
-    set = set[(np.abs(stats.zscore(set)) < threshold).all(axis=1)]
+    # print("\n---- After imputing missing values")
+    # print("\nhead:")
+    # print(set.head(20))
+    # # print("\ntail:")
+    # # print(set.tail())
+    # print("shape:")
+    # print(set.shape)
 
-    print("\n---- After eliminating outliers")
-    print("\nhead:")
-    print(set.head())
-    # print("\ntail:")
-    # print(set.tail())
-    print("shape:")
-    print(set.shape)
-
-    return set
-
-# combine X_train and y_train for data cleaning
-train_set = pd.concat([X_train,y_train], axis=1)
-print("\n---- Train set after concatenating X_train and y_train")
-print(train_set)
+    return set, mode_rt, mean, mode_wgd, mode_wdn, mode_wdt
+    # --------------------------------------------------------------------------------------------
 
 # call the function for data cleaning
-cleaned_train_set = cleanData(train_set)
-print("\n---- After data cleaning for training set:")
-print(cleaned_train_set)
+cleaned_train_set, mode_rt, mean, mode_wgd, mode_wdn, mode_wdt = cleanData(train_set, mode_rt, mean, mode_wgd, mode_wdn, mode_wdt, 'train')
+# print("\n---- After data cleaning for training set:")
+# print(cleaned_train_set)
+
+
+# detect and eliminate outliers using z-score
+threshold = 3  # drop records greater than this value
+cleaned_train_set = cleaned_train_set[(np.abs(stats.zscore(cleaned_train_set)) < threshold).all(axis=1)]
+
+# print("\n---- After eliminating outliers")
+# print("\nhead:")
+# print(cleaned_train_set.head())
+# # print("\ntail:")
+# # print(cleaned_train_set.tail())
+# print("shape:")
+# print(cleaned_train_set.shape)
+
 
 # split X_train and y_train
 cleaned_X_train = cleaned_train_set.iloc[:,:-1]                   # all columns except the last, RainTomorrow
 cleaned_y_train = cleaned_train_set['RainTomorrow'].to_frame()    # RainTomorrow
-print("X_train:")
-print(cleaned_X_train)
-print("y_train:")
-print(cleaned_y_train)
+# print("X_train:")
+# print(cleaned_X_train)
+# print("y_train:")
+# print(cleaned_y_train)
+
 
 # == NAIVE BAYES =================================================================================
+# Training ---------------------------------------------------------------------------------------
+model = GaussianNB()
+model.fit(cleaned_X_train.values, cleaned_y_train.values.ravel())
+
+# Testing ----------------------------------------------------------------------------------------
+# Data Preprocessing
+# combine X_test and y_test for data cleaning
+test_set = pd.concat([X_test,y_test], axis=1)
+
+# call the function for data cleaning
+# print(f'mode_rt for test: {mode_rt}')
+# print(f'mean for test: {mean}')
+# print(f'mode_wgd for test: {mode_wgd}')
+# print(f'mode_wdn for test: {mode_wdn}')
+# print(f'mode_wdt for test: {mode_wdt}')
+cleaned_test_set, mode_rt, mean, mode_wgd, mode_wdn, mode_wdt = cleanData(test_set, mode_rt, mean, mode_wgd, mode_wdn, mode_wdt, 'test')
+
+# split X_train and y_train
+cleaned_X_test = cleaned_test_set.iloc[:,:-1]                   # all columns except the last, RainTomorrow
+cleaned_y_test = cleaned_test_set['RainTomorrow'].to_frame()    # RainTomorrow
+
+# print("\ncleaned_X_test head:")
+# print(cleaned_X_test.head(40))
+# print("\ncleaned_X_test tail:")
+# print(cleaned_X_test.tail(40))
+
+# predict
+y_pred = model.predict(cleaned_X_test.values)
+
+# display results
+print("y_pred")
+print(y_pred)
+
+print("\ny_test")
+print(cleaned_y_test)
+
+# Evaluate Performance ---------------------------------------------------------------------------
+cm = confusion_matrix(cleaned_y_test, y_pred)
+print("\nConfusion matrix:")
+print(cm)
+
+cr = classification_report(cleaned_y_test, y_pred)
+print("\nClassification report:")
+print(cr)
